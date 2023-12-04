936386e02a97da2a69fd8dedbac19dac57811f88
spenserg92 <spenserg92@gmail.com>
2023-12-03 23:30:11 -0500

    adds a handleMove function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 	/*----- constants -----*/
     // 1) Define required constants:
     // 1.1) Define a colors object with keys of 'null' (when the square is empty), and players 1 & -1. The value assigned to each key represents the color to display for an empty square (null), player 1 and player -1.
-    // 1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
 const colors = {
     0: 'white',
     1: 'magenta',
     '-1': 'teal'
 }
+
+//1.2) Define the 8 possible winning combinations, each containing three indexes of the board that make a winner if they hold the same player value.
+
+
 const winSolution = [
     [0, 1, 2],
     [3, 4, 5],
@@ -19,19 +22,19 @@ const winSolution = [
 ]
 console.log(winSolution)
 	/*----- state variables -----*/
-
+//2.1) Use a board array to represent the squares.
 let board
+//2.2) Use a turn variable to remember whose turn it is.
 let turn
+//  2.3) Use a winner variable to represent three different possibilities - player that won, a tie, or game in play.
 let winner 
 
 function init(){
     turn = 1
     winner = null
     board = [
-        [0, 0, 0], //col 0
-        [0, 0, 0], //col 1
-        [0, 0, 0], //col 2
-    ]
+        null, null, null, null, null, null, null, null, null
+    ];
     render()
     
 }
@@ -45,9 +48,22 @@ init()
  // 3.1) Store the 9 elements that represent the squares on the page.
 const messageEl = document.querySelector('h2')
 const newGameButton = document.querySelector('button')
-let boxes = [...document.querySelectorAll('#board > div')]
+
 
 	/*----- event listeners -----*/
-
-
+document.getElementById('board').addEventListener('click', handleMove);
+newGameButton.addEventListener('click', init)
 	/*----- functions -----*/
+
+    function handleMove(evt){
+        const square = parseInt(evt.target.id.replace('sq-', ''));
+        if (
+            isNaN(square) || board[square] || winner
+        ) return;
+
+        board[square] = turn;
+        turn *= -1;
+        winner = getWinner();
+
+        render()
+    }
